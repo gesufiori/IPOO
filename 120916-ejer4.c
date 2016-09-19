e5fe5bbed40531228994e998a079d3a6817622dd
gesufiori <gesufiori@gmail.com>
2016-09-19 18:13:14 -0500

    Add files via upload

---
+++ 120916-ejer4.c
@@ -0,0 +1,15 @@
+/*ejercicio numero 4*/
+#include <stdio.h>
+#include <string.h>
+
+void inversa(char str[], int size){
+    if (size!=EOF){
+        printf("%c",str[size]);
+        inversa(str,size-1);
+    }
+}
+int main(){
+    char inv_str[]="inversa";
+    inversa(inv_str,strlen(inv_str));
+    return 0;
+}
